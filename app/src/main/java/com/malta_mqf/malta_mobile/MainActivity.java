d1cab500782d013d799ad26cd4dff7bb2cbc382a
ManojSoti <manoj.rs@suveechi.in>
2026-05-20 17:56:17 +0530

    Handling null pointer exception

--- app/src/main/java/com/malta_mqf/malta_mobile/MainActivity.java
+++ app/src/main/java/com/malta_mqf/malta_mobile/MainActivity.java
@@ -3908,7 +3908,9 @@ public class MainActivity extends BaseActivity {
             @Override
             protected Void doInBackground(Void... voids) {
                 Cursor cursor = submitOrderDB.getalldirectbillinginvoices();
+
                 int totalinvoiceno = cursor.getCount();
+                System.out.println("invoices count for direct billing: "+totalinvoiceno);
 
                 if (totalinvoiceno == 0) {
                     runOnUiThread(() -> showNodirectbillinginvoiceDialog());
@@ -3982,7 +3984,7 @@ public class MainActivity extends BaseActivity {
             protected Void doInBackground(Void... voids) {
                 Cursor cursor = returnDB.getalldirectbillinginvoices();
                 int totalinvoiceno = cursor.getCount();
-
+                System.out.println("creditnote count for direct billing: "+totalinvoiceno);
                 if (totalinvoiceno == 0) {
                     runOnUiThread(() -> showNodirectbillinginvoiceDialog());
                     return null;
