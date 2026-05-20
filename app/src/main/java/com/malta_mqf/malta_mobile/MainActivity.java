425e182872e2095ce55b7e69f904cf1ee37873ca
Admin <surabhi.mp@suveechi.in>
2026-05-20 13:33:04 +0530

    INITIAL COMMIT new production apk 2026-05-21

--- app/src/main/java/com/malta_mqf/malta_mobile/MainActivity.java
+++ app/src/main/java/com/malta_mqf/malta_mobile/MainActivity.java
@@ -1180,7 +1180,7 @@ public class MainActivity extends BaseActivity {
             }
             Log.d("UserID", userID);
             System.out.println("vehicle" + vehiclenum+"   ");
-            userName.setText(name +"     "+" 20-05-2026");//check for url
+            userName.setText(name +"     "+" 21-05-2026");//check for url
             emailId.setText(email);
             empCode.setText(vehiclenum);
         }
